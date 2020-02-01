e3baee15354d43486a0ba379cec2bba19ae2257b
羽田 佳啓 <punio@outlook.com>
2020-02-01 17:51:16 +0900

    1日の集計時に前月を計算していたんだけどその場合当月の計算をしないので両方するように(たぶん)した

--- Web/DataWriter/AggregateOrchestrator.cs
+++ Web/DataWriter/AggregateOrchestrator.cs
@@ -56,199 +56,204 @@ namespace DataWriter
 		{
 			var logger = activityContext.GetInput<string>();
 
+			var targetDayList = new List<DateTime>();
 			#region 集計 (前日までの月単位)
-			var to = DateTime.UtcNow.AddDays(-1);
-			if (to.Day <= 1)
+			var toDay = DateTime.UtcNow.AddDays(-1);
+			if (toDay.Day <= 1)
 			{
-				to = to.AddMonths(-1);
+				targetDayList.Add(toDay.AddMonths(-1));
 			}
-			to = new DateTime(to.Year, to.Month, DateTime.DaysInMonth(to.Year, to.Month), 23, 59, 59, DateTimeKind.Utc);
-			var from = new DateTime(to.Year, to.Month, 1, 0, 0, 0, DateTimeKind.Utc);
-			var rowKeyTo = $"{DateTime.MaxValue.Ticks - from.Ticks:d19}_{Guid.Empty}";
-			var rowKeyFrom = $"{DateTime.MaxValue.Ticks - (to.Ticks + 600000000):d19}_ffffffff-ffff-ffff-ffff-ffffffffffff";  // TimeSpan.FromMinutes(1).Ticks
+			targetDayList.Add(toDay);
 			#endregion
 
-			log.LogInformation($"Correct : {from} - {to}");
+			foreach (var targetDay in targetDayList) {
+				var to = new DateTime(targetDay.Year, targetDay.Month, DateTime.DaysInMonth(targetDay.Year, targetDay.Month), 23, 59, 59, DateTimeKind.Utc);
+				var from = new DateTime(to.Year, to.Month, 1, 0, 0, 0, DateTimeKind.Utc);
+				var rowKeyTo = $"{DateTime.MaxValue.Ticks - from.Ticks:d19}_{Guid.Empty}";
+				var rowKeyFrom = $"{DateTime.MaxValue.Ticks - (to.Ticks + 600000000):d19}_ffffffff-ffff-ffff-ffff-ffffffffffff";  // TimeSpan.FromMinutes(1).Ticks
 
-			var table = await binder.BindAsync<CloudTable>(new TableAttribute("SwingData"));
-			var query = new TableQuery<SwingDataEntity>();
-			query.FilterString = TableQuery.CombineFilters(
-				TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, logger),
-				TableOperators.And,
-				TableQuery.CombineFilters(
-					TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.GreaterThanOrEqual, rowKeyFrom),
+				log.LogInformation($"Correct : {from} - {to}");
+
+				var table = await binder.BindAsync<CloudTable>(new TableAttribute("SwingData"));
+				var query = new TableQuery<SwingDataEntity>();
+				query.FilterString = TableQuery.CombineFilters(
+					TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, logger),
 					TableOperators.And,
-					TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.LessThanOrEqual, rowKeyTo)));
+					TableQuery.CombineFilters(
+						TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.GreaterThanOrEqual, rowKeyFrom),
+						TableOperators.And,
+						TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.LessThanOrEqual, rowKeyTo)));
 
-			TableContinuationToken token = null;
-			var dataEntities = new List<SwingDataEntity>();
-			do
-			{
-				var entities = await table.ExecuteQuerySegmentedAsync(query, token);
-				dataEntities.AddRange(entities.Where(e => e.BallSpeed > 0));
-				token = entities.ContinuationToken;
-			} while (token != null);
-
-			var temp = dataEntities.ToArray();
-			if (temp.Length <= 0) return;
-
-			var tableOperations = new TableBatchOperation();
-
-			#region 集計
-			var summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.TotalDistance}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.TotalDistance,
-				Result = temp.Sum(d => d.Distance)
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-			summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MaxHeadSpeed}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.MaxHeadSpeed,
-				Result = temp.Max(d => d.HeadSpeed)
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-			summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MinHeadSpeed}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.MinHeadSpeed,
-				Result = temp.Min(d => d.HeadSpeed)
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-			summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MaxMeetRate}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.MaxMeetRate,
-				Result = temp.Max(d => d.Meet)
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-			summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MinMeetRate}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.MinMeetRate,
-				Result = temp.Min(d => d.Meet)
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-			summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.TotalBalls}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.TotalBalls,
-				Result = temp.Length
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-			summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MaxDistance}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.MaxDistance,
-				Result = temp.Max(d => d.Distance)
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-			summary = new SwingSummaryEntity
-			{
-				PartitionKey = $"{from:yyyyMM}",
-				RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MinDistance}",
-				DeviceId = logger,
-				Type = (int)SwingSummaryEntity.SummaryType.MinDistance,
-				Result = temp.Min(d => d.Distance)
-			};
-			tableOperations.Add(TableOperation.InsertOrMerge(summary));
-
-
-			var summaryTable = await binder.BindAsync<CloudTable>(new TableAttribute("SwingSummary"));
-			await summaryTable.CreateIfNotExistsAsync();
-			await summaryTable.ExecuteBatchAsync(tableOperations);
-			tableOperations.Clear();
-			#endregion
-
-			#region 統計
-			// クラブ毎の平均値とか
-			foreach (var club in Enum.GetValues(typeof(ClubType)))
-			{
-				var clubData = temp.Where(s => s.Club == (int)club).ToArray();
-				if (clubData.Length <= 0) continue;
-
-				var stat = new SwingStatisticsEntity
+				TableContinuationToken token = null;
+				var dataEntities = new List<SwingDataEntity>();
+				do
 				{
-					PartitionKey = logger,
-					RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.HeadSpeedAverage}_{club}",
-					Time = from,
-					Club = (int)club,
-					Type = (int)SwingStatisticsEntity.StatisticsType.HeadSpeedAverage,
-					Result = Average(clubData.Select(c => c.HeadSpeed).ToArray())
-				};
-				tableOperations.Add(TableOperation.InsertOrMerge(stat));
+					var entities = await table.ExecuteQuerySegmentedAsync(query, token);
+					dataEntities.AddRange(entities.Where(e => e.BallSpeed > 0));
+					token = entities.ContinuationToken;
+				} while (token != null);
 
-				stat = new SwingStatisticsEntity
-				{
-					PartitionKey = logger,
-					RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.BallSpeedAverage}_{club}",
-					Time = from,
-					Club = (int)club,
-					Type = (int)SwingStatisticsEntity.StatisticsType.BallSpeedAverage,
-					Result = Average(clubData.Select(c => c.BallSpeed).ToArray())
-				};
-				tableOperations.Add(TableOperation.InsertOrMerge(stat));
+				var temp = dataEntities.ToArray();
+				if (temp.Length <= 0) return;
 
-				stat = new SwingStatisticsEntity
-				{
-					PartitionKey = logger,
-					RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.DistanceAverage}_{club}",
-					Time = from,
-					Club = (int)club,
-					Type = (int)SwingStatisticsEntity.StatisticsType.DistanceAverage,
-					Result = Average(clubData.Select(c => c.Distance).ToArray())
-				};
-				tableOperations.Add(TableOperation.InsertOrMerge(stat));
+				var tableOperations = new TableBatchOperation();
 
-				stat = new SwingStatisticsEntity
+				#region 集計
+				var summary = new SwingSummaryEntity
 				{
-					PartitionKey = logger,
-					RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.MeetAverage}_{club}",
-					Time = from,
-					Club = (int)club,
-					Type = (int)SwingStatisticsEntity.StatisticsType.MeetAverage,
-					Result = Average(clubData.Select(c => c.Meet).ToArray())
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.TotalDistance}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.TotalDistance,
+					Result = temp.Sum(d => d.Distance)
 				};
-				tableOperations.Add(TableOperation.InsertOrMerge(stat));
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
 
-				stat = new SwingStatisticsEntity
+				summary = new SwingSummaryEntity
 				{
-					PartitionKey = logger,
-					RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.TotalBalls}_{club}",
-					Time = from,
-					Club = (int)club,
-					Type = (int)SwingStatisticsEntity.StatisticsType.TotalBalls,
-					Result = clubData.Count()
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MaxHeadSpeed}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.MaxHeadSpeed,
+					Result = temp.Max(d => d.HeadSpeed)
 				};
-				tableOperations.Add(TableOperation.InsertOrMerge(stat));
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
+
+				summary = new SwingSummaryEntity
+				{
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MinHeadSpeed}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.MinHeadSpeed,
+					Result = temp.Min(d => d.HeadSpeed)
+				};
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
+
+				summary = new SwingSummaryEntity
+				{
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MaxMeetRate}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.MaxMeetRate,
+					Result = temp.Max(d => d.Meet)
+				};
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
+
+				summary = new SwingSummaryEntity
+				{
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MinMeetRate}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.MinMeetRate,
+					Result = temp.Min(d => d.Meet)
+				};
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
+
+				summary = new SwingSummaryEntity
+				{
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.TotalBalls}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.TotalBalls,
+					Result = temp.Length
+				};
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
+
+				summary = new SwingSummaryEntity
+				{
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MaxDistance}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.MaxDistance,
+					Result = temp.Max(d => d.Distance)
+				};
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
+
+				summary = new SwingSummaryEntity
+				{
+					PartitionKey = $"{from:yyyyMM}",
+					RowKey = $"{logger}_{SwingSummaryEntity.SummaryType.MinDistance}",
+					DeviceId = logger,
+					Type = (int)SwingSummaryEntity.SummaryType.MinDistance,
+					Result = temp.Min(d => d.Distance)
+				};
+				tableOperations.Add(TableOperation.InsertOrMerge(summary));
+
+
+				var summaryTable = await binder.BindAsync<CloudTable>(new TableAttribute("SwingSummary"));
+				await summaryTable.CreateIfNotExistsAsync();
+				await summaryTable.ExecuteBatchAsync(tableOperations);
+				tableOperations.Clear();
+				#endregion
+
+				#region 統計
+				// クラブ毎の平均値とか
+				foreach (var club in Enum.GetValues(typeof(ClubType)))
+				{
+					var clubData = temp.Where(s => s.Club == (int)club).ToArray();
+					if (clubData.Length <= 0) continue;
+
+					var stat = new SwingStatisticsEntity
+					{
+						PartitionKey = logger,
+						RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.HeadSpeedAverage}_{club}",
+						Time = from,
+						Club = (int)club,
+						Type = (int)SwingStatisticsEntity.StatisticsType.HeadSpeedAverage,
+						Result = Average(clubData.Select(c => c.HeadSpeed).ToArray())
+					};
+					tableOperations.Add(TableOperation.InsertOrMerge(stat));
+
+					stat = new SwingStatisticsEntity
+					{
+						PartitionKey = logger,
+						RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.BallSpeedAverage}_{club}",
+						Time = from,
+						Club = (int)club,
+						Type = (int)SwingStatisticsEntity.StatisticsType.BallSpeedAverage,
+						Result = Average(clubData.Select(c => c.BallSpeed).ToArray())
+					};
+					tableOperations.Add(TableOperation.InsertOrMerge(stat));
+
+					stat = new SwingStatisticsEntity
+					{
+						PartitionKey = logger,
+						RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.DistanceAverage}_{club}",
+						Time = from,
+						Club = (int)club,
+						Type = (int)SwingStatisticsEntity.StatisticsType.DistanceAverage,
+						Result = Average(clubData.Select(c => c.Distance).ToArray())
+					};
+					tableOperations.Add(TableOperation.InsertOrMerge(stat));
+
+					stat = new SwingStatisticsEntity
+					{
+						PartitionKey = logger,
+						RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.MeetAverage}_{club}",
+						Time = from,
+						Club = (int)club,
+						Type = (int)SwingStatisticsEntity.StatisticsType.MeetAverage,
+						Result = Average(clubData.Select(c => c.Meet).ToArray())
+					};
+					tableOperations.Add(TableOperation.InsertOrMerge(stat));
+
+					stat = new SwingStatisticsEntity
+					{
+						PartitionKey = logger,
+						RowKey = $"{from:yyyyMM}_{SwingStatisticsEntity.StatisticsType.TotalBalls}_{club}",
+						Time = from,
+						Club = (int)club,
+						Type = (int)SwingStatisticsEntity.StatisticsType.TotalBalls,
+						Result = clubData.Count()
+					};
+					tableOperations.Add(TableOperation.InsertOrMerge(stat));
+				}
+				var statTable = await binder.BindAsync<CloudTable>(new TableAttribute("SwingStatistics"));
+				await statTable.CreateIfNotExistsAsync();
+				await statTable.ExecuteBatchAsync(tableOperations);
+				tableOperations.Clear();
+				#endregion
 			}
-			var statTable = await binder.BindAsync<CloudTable>(new TableAttribute("SwingStatistics"));
-			await statTable.CreateIfNotExistsAsync();
-			await statTable.ExecuteBatchAsync(tableOperations);
-			tableOperations.Clear();
-			#endregion
 		}
 
 		private static double Average(int[] values)
