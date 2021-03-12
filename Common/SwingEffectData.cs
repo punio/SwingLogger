2358b317667b050f418f2421b1be08d6f4435116
Hada Yoshihiro <punio@outlook.com>
2021-03-12 17:11:52 +0900

    ver. 1.2

---
+++ Common/SwingEffectData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwingCommon
+{
+	public class SwingEffectData : SwingData
+	{
+		public float MinMeetRate { get; set; }
+		public float MaxMeetRate { get; set; }
+		public float MaxDistance { get; set; }
+	}
+}
